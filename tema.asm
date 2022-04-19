2c742183ec210fbcbede807e985e3fe45074c63c
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2022-04-19 10:25:07 +0300

    Add files via upload

--- tema.asm
+++ tema.asm
@@ -1,26 +1,26 @@
 SECTION .data
     formatin: db "%d", 0
-    formatout: db "Nr impar: %d"
-
+    formatout: db "Cel mai apropiat nr par este: %d", 10, 0 
+ 
     a: times 4 db 0 
-    msg1 db  "a =   ", 0xA,0xD 
+    msg1 db  "Enter first number:  ", 0xA,0xD 
     len1 equ $ - msg1
-
+ 
 SECTION .text
    global _main 
    extern scanf 
    extern printf 
-
+ 
    global main
-
+ 
 main:
-
+ 
     mov eax, 4	
     mov ebx, 1    
     mov ecx, msg1
     mov edx, len1
     int 80h    
-    
+ 
     ;citire a
     sub esp, 4
    mov [esp], dword a
@@ -28,36 +28,29 @@ main:
    mov [esp], dword formatin
    call scanf
    add esp, 8
-
+ 
     ; verificare paritate
     mov ax, a
     and ax, 1
-	not ax, 1
-    jz odd
+    jz evnn
     mov eax, 4
     mov ebx, 1
     jz incr
     int 0x80
-
-    odd:
-    mov ah, 09h ;
+ 
+    evnn:
+    mov ah, 09h ;daca e par, il afiseaza
     mov eax, 4
     mov ebx, 1
-    mov ebx, dword [fin]
-    sub esp, 4
-    mov [esp], ebx
-    sub esp, 4
-    mov [esp], dword formatout
-    call printf                            
-    add esp, 8
+    jz afisare_par
     int 0x80
     outprog:
     mov eax, 1
     int 0x80
-
+ 
     incr:
     mov eax, 0
-    add eax, 1 
+    add eax, 1 ;daca e impar incerementeaza, apoi afiseaza cel mai apropiat nr par
     inc eax
     mov ebx, dword [fin]
     sub esp, 4
@@ -66,5 +59,12 @@ main:
     mov [esp], dword formatout
     call printf                            
     add esp, 8
-
-    
+ 
+    afisare_par:
+    mov ebx, dword [fin]
+   sub esp, 4
+   mov [esp], ebx
+   sub esp, 4
+   mov [esp], dword formatout
+   call printf                            
+   add esp, 8
